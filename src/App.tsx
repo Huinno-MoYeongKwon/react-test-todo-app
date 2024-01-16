c8e051e37e251e8ee768b8e51524eb883efaabf4
Huinno-MoYeongKwon <149992412+Huinno-MoYeongKwon@users.noreply.github.com>
2024-01-16 15:42:58 +0900

    init project : react, react-redux, redux-toolkit

---
+++ src/App.tsx
@@ -0,0 +1,43 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+import React, { useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import {
+  decrement,
+  increment,
+  incrementByAmount,
+  incrementAsync,
+  selectCount,
+} from "./features/counter/counterSlice";
+
+function App() {
+  const count = useSelector(selectCount);
+  const dispatch = useDispatch();
+  const [incrementAmount, setIncrementAmount] = useState(0);
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>{count}</h1>
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+
+        <button onClick={() => dispatch(increment())}> 즈응가</button>
+        <button onClick={() => dispatch(decrement())}> 가암소</button>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/features/counter/counterSlice.ts
@@ -0,0 +1,38 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+
+interface CounterState {
+  value: number;
+}
+
+const initialState: CounterState = {
+  value: 0,
+};
+
+const slice = createSlice({
+  name: "counter",
+  initialState,
+  reducers: {
+    increment: (state) => {
+      state.value += 1;
+    },
+    decrement: (state) => {
+      state.value -= 1;
+    },
+    incrementByAmount: (state, action: PayloadAction<number>) => {
+      state.value += action.payload;
+    },
+  },
+});
+
+export const { increment, decrement, incrementByAmount } = slice.actions;
+
+export const incrementAsync = (amount: number) => (dispatch: any) => {
+  setTimeout(() => {
+    dispatch(incrementByAmount(amount));
+  }, 1000);
+};
+
+export const selectCount = (state: { counter: CounterState }) =>
+  state.counter.value;
+
+export default slice.reducer;
