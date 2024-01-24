338e9928cbf59f40aead6e8cdf24b582cba1fdfd
Huinno-MoYeongKwon <149992412+Huinno-MoYeongKwon@users.noreply.github.com>
2024-01-24 18:05:45 +0900

    테스트

--- src/App.tsx
+++ src/App.tsx
@@ -23,6 +23,61 @@ function App() {
 
   return (
     <div className="App">
+      <button
+        onClick={() => {
+          const fileLinks = [
+            "http://localhost:3000/test1.jpg",
+            "http://localhost:3000/test2.jpg",
+            "http://localhost:3000/test3.jpg",
+          ];
+
+          fileLinks.forEach((fileLink, index) => {
+            const link = document.createElement("a");
+            link.href = fileLink;
+            link.download = index + ".jpg";
+            link.style.display = "none";
+            document.body.appendChild(link);
+            link.click();
+
+            setTimeout(() => {
+              URL.revokeObjectURL(fileLink);
+              link.remove();
+            }, 1000);
+          });
+        }}
+      >
+        다운로드
+      </button>
+
+      <a
+        href="http://localhost:3000/test1.jpg"
+        download
+        target="_blank"
+        rel="noreferrer"
+      >
+        test 이미지1
+      </a>
+      <br></br>
+
+      <a
+        href="http://localhost:3000/test2.jpg"
+        download
+        target="_blank"
+        rel="noreferrer"
+      >
+        test 이미지2
+      </a>
+
+      <br></br>
+      <a
+        href="http://localhost:3000/test3.jpg"
+        download
+        target="_blank"
+        rel="noreferrer"
+      >
+        test 이미지1
+      </a>
+
       {/* <header className="App-header"></header> */}
       <h1>{count}</h1>
       <img src={logo} className="App-logo" alt="logo" />
